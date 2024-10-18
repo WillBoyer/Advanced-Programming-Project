fb61112a59132c9a8204daeb74c8fb45b6919b84
William Boyer <w.boyer@outlook.com>
2024-10-18 21:31:22 +0100

    Replaced initial directory structure, with a new structure composed of a C# app for the frontend and an F# library for the interpreter.

---
+++ CSharpApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.FSharp.Core;
+using Microsoft.FSharp.Collections;
+
+namespace CSharpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple Interpreter");
+            Console.Write("Enter an Expression: ");
+            string input = Console.ReadLine();
+            int result = Interpreter.interpret(input);
+            Console.WriteLine("Final result = {0}", result);
+            Console.ReadLine();
+        }
+    }
+}
