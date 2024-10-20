8293aeec844114403a9d055fa99bfd8e5e2fa042
Rijo <Rijo.Thomas@uea.ac.uk>
2024-10-21 04:51:04 +0530

    Arithmetic operations and basic window

---
+++ CSharpInterpreterGUI/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using Microsoft.FSharp.Collections;
+
+
+namespace CSharpInterpreterGUI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Evaluate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                
+                string input = inputTextBox.Text;
+                
+                int result = ArithmeticInterpreter.evaluateExpression(input);
+                
+                resultTextBlock.Text = $"Result: {result}";
+            }
+            catch (Exception ex)
+            {
+                resultTextBlock.Text = $"Error: {ex.Message}";
+            }
+        }
+    }
+}
---
+++ FSharpLib/Library.fs
@@ -0,0 +1,9 @@
+﻿module SumFloat
+
+let rec sum (list: list<double>) =
+    match list with
+    | (head: double) :: (tail: list<double>) -> head + sum(tail)
+    | [] -> 0.0
+
+
+
